5468b9a732e7bb230a18a4a8d49a7806d6938e1f
rubanenkos <sergii.rubanenko@gmail.com>
2024-09-21 16:37:58 +0300

    updated test1

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,4 +6,8 @@ def test_has_title1(page: Page):
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
+    page.get_by_role("button", name="Node.js").hover()
+    page.get_by_label("Main", exact=True).get_by_role("link", name="Python").click()
+    expect(page).to_have_url(re.compile(r".*/python/?"))
+
 
