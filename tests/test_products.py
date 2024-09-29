6558e6d9b452bc73c5b2eb9fd52037e72f3c576b
rubanenkos <sergii.rubanenko@gmail.com>
2024-09-29 14:15:45 +0300

    Added test test_check_purchase_has_correct_price

--- tests/test_products.py
+++ tests/test_products.py
@@ -3,13 +3,13 @@ from constants.users import Users
 
 class TestProducts:
 
-    def test_check_purchase_has_correct_price(self, login_page, products_page, cart_page):
-        purchase_item = "Sauce Labs Backpack"
+    def test_check_product_price_in_cart_is_correct(self, login_page, products_page, cart_page):
+        product_name = "Sauce Labs Backpack"
         login_page.login(Users.STANDARD_USER)
-        purchase_price = products_page.pick_purchase(purchase_item)
+        product_price = products_page.pick_purchase(product_name)
         products_page.header.open_cart()
-        cart_page.check_is_purchase_in_cart(purchase_item)
-        cart_page.check_is_purchase_has_correct_price(purchase_item, purchase_price)
+        cart_page.check_is_purchase_in_cart(product_name)
+        cart_page.check_is_purchase_has_correct_price(product_name, product_price)
 
 
 
